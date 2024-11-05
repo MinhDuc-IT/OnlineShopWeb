ae7bd97c145d1f45e8ae39933f7adfd531c61890
lightBringer-c <chau24628@gmail.com>
2024-11-05 22:41:01 +0700

    #6-thống kê doanh thu và sản phẩm

--- OnlineShopWeb/App_Start/RouteConfig.cs
+++ OnlineShopWeb/App_Start/RouteConfig.cs
@@ -12,11 +12,17 @@ namespace OnlineShopWeb
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapRoute(
+              name: "CategoryProduct",
+              url: "danh-muc-san-pham/{id}",
+              defaults: new { controller = "Product", action = "ProductCategory", id = UrlParameter.Optional },
+              namespaces: new[] { "OnlineShopWeb.Controllers" }
+          );
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "OnlineShopWeb.Controllers" }
             );
         }
     }
---
+++ OnlineShopWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,105 @@
+﻿using OnlineShopWeb.Data;
+using OnlineShopWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShopWeb.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Admin/Category
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            IEnumerable<Category> categories = db.Categories.ToList();
+            return View(categories);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult createCategory()
+        {
+            try
+            {
+                string Name = Request.Form["Name"];
+                string Description = Request.Form["Description"];
+
+                Category newObj = new Category();
+                newObj.Name = Name;
+                newObj.Description = Description;
+
+                db.Categories.Add(newObj);
+                db.SaveChanges();
+
+                return Json(new { success = true, message = "Thêm mới danh mục thành công" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        public ActionResult Edit(int id)
+        {
+            var category = db.Categories.Find(id);
+
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(category);
+        }
+        [HttpPost]
+        public ActionResult editCategory()
+        {
+            try
+            {
+                int categoryId = int.Parse(Request.Form["ID"]);
+                var category = db.Categories.FirstOrDefault(x => x.CategoryId == categoryId);
+                if (category == null)
+                {
+                    return Json(new { success = false, message = "Không tìm thấy danh mục" }, JsonRequestBehavior.AllowGet);
+                }
+
+                // Lấy giá trị từ form
+                string Name = Request.Form["Name"];
+                string Description = Request.Form["Description"];
+                bool IsDeleted = bool.Parse(Request.Form["IsDelete"]);
+
+                // Cập nhật thuộc tính
+                category.Name = Name;
+                category.Description = Description;
+                category.IsDeleted = IsDeleted;
+
+                db.Entry(category).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+                return Json(new { success = true, message = "Sửa danh mục thành công" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var item = db.Categories.Find(id);
+                item.IsDeleted = true;
+                db.SaveChanges();
+                return Json(new { success = true, });
+            }
+            catch
+            {
+                return Json(new { success = false, });
+            }
+        }
+    }
+}
